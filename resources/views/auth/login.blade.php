c4e18f3c43a8d95f8bc6a8f6e49fcd90768df341
patahgaming <aliifprabu4590@gmail.com>
2024-08-22 09:38:44 +0700

    dashboard dan aviews

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,14 +1,39 @@
-<form method="POST" action="{{ route('login') }}">
-    @csrf
-    <div>
-        <label for="email">Email</label>
-        <input type="email" name="email" required autofocus>
-    </div>
 
-    <div>
-        <label for="password">Password</label>
-        <input type="password" name="password" required>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="btw.css">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="{{ asset('css/auth.css') }}">
+</head>
+<body>
+    <div class="wrepper">
+        <form method="POST" action="{{ route('login') }}">
+            @csrf
+            <h1>Login</h1>
+            <div class="input-box">
+                <!-- Menggunakan 'name' dan 'type' yang sesuai agar form berfungsi -->
+                <input type="email" name="email" placeholder="Username/Email" required autofocus>
+                <i class='bx bxs-user'></i>
+            </div>
+            <div class="input-box">
+                <!-- Menggunakan 'name' dan 'type' yang sesuai agar form berfungsi -->
+                <input type="password" name="password" placeholder="Password" required>
+                <i class='bx bx-lock'></i>
+            </div>
+            <div class="remember-forgot">
+                <label><input type="checkbox" name="remember">Ingatkan saya</label>
+                <a href="#">Lupa password?</a>
+            </div>
+            <!-- Pastikan 'type' adalah 'submit' agar button bisa submit form -->
+            <button type="submit" class="btn">Login</button>
+            <div class="register-link">
+                <p>Tidak punya akun? <a href="/register">Daftar</a></p>
+            </div>
+        </form>
     </div>
-
-    <button type="submit">Login</button>
-</form>
+</body>
+</html>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,24 +1,46 @@
-<form method="POST" action="{{ route('register') }}">
-    @csrf
-    <div>
-        <label for="name">Name</label>
-        <input type="text" name="name" required autofocus>
-    </div>
 
-    <div>
-        <label for="email">Email</label>
-        <input type="email" name="email" required>
-    </div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="btw.css">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="{{ asset('css/auth.css') }}">
+</head>
+<body>
+    <div class="wrepper">
+        <form method="POST" action="{{ route('register') }}">
+            @csrf
+            <h1>Register</h1>
 
-    <div>
-        <label for="password">Password</label>
-        <input type="password" name="password" required>
-    </div>
+            <div class="input-box">
+                <input type="text" name="name" placeholder="Name" required autofocus>
+                <i class='bx bxs-user'></i>
+            </div>
 
-    <div>
-        <label for="password_confirmation">Confirm Password</label>
-        <input type="password" name="password_confirmation" required>
-    </div>
+            <div class="input-box">
+                <input type="email" name="email" placeholder="Email" required>
+                <i class='bx bxs-envelope'></i>
+            </div>
 
-    <button type="submit">Register</button>
-</form>
+            <div class="input-box">
+                <input type="password" name="password" placeholder="Password" required>
+                <i class='bx bx-lock'></i>
+            </div>
+
+            <div class="input-box">
+                <input type="password" name="password_confirmation" placeholder="Confirm Password" required>
+                <i class='bx bx-lock'></i>
+            </div>
+
+            <button type="submit" class="btn">Register</button>
+
+            <div class="register-link">
+                <p>Sudah punya akun? <a href="/login">Login</a></p>
+            </div>
+        </form>
+    </div>
+</body>
+</html>
